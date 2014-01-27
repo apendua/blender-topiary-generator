0ed66c2859e9eb5fde78f81c150165aec5c21f2e
Tomasz Lenarcik <apendua@gmail.com>
2014-01-27 22:07:10 +0100

    Adopting the code to arbitrary number of vertices

--- object_topiary_generate/topiary.py
+++ object_topiary_generate/topiary.py
@@ -7,39 +7,60 @@ from mathutils import Vector, Matrix
 
 def generate_tree_mesh(base_obj, ref_obj, iterations=4):
 
-  def project(vertex, center):
+  def project(vertex, center_of_projection):
     intersection_point, _, index = \
-        ref_obj.ray_cast(center, vertex + 10 * (vertex - center).normalized())
+        ref_obj.ray_cast(center_of_projection,
+          vertex + 10 * (vertex - center_of_projection).normalized())
     if index >= 0:
       return intersection_point
     return vertex   
   
-  def do_recursive(root, triangle, level, projected=None):
-  
-    # if len(triangle) != 3:
-    #  raise ValueError
+  def do_triangle():
+    pass
     
-    if projected is None:
-      projected = [project(vert.co, root) for vert in triangle]
-
+  def do_quad():
+    pass
+    
+  def do_polygon():
+    pass
+  
+  def do_recursive(root, polygon, level, projected=None):
+  
+    # if len(polygon) != 3:
+    #  raise ValueError
+ 
+    p = projected # convenience alias
+ 
+    if p is None:
+      p = [project(vert.co, root) for vert in polygon]
+    
+    nVerts = len(polygon)
+    
     # find center
-    alpha = (0.9 + random.random()) / 3 # randomize
-    center = alpha * root
-    for vert in projected[:3]:
-      center += (1 - alpha)/3 * vert
-        
+    center = Vector((0,0,0))
+    for vert in p[:nVerts]:
+      center += 1/nVerts * vert
+
+    # and new root
+    alpha = (0.9 + random.random()) / nVerts # randomize
+    newRoot = (1-alpha) * center + alpha * root
+      
     # subdivide
-    projected.append(project(1/2*(projected[0] + projected[1]), center))
-    projected.append(project(1/2*(projected[1] + projected[2]), center))
-    projected.append(project(1/2*(projected[2] + projected[0]), center))
-        
+    if nVerts <= 4:
+      p.append(p[0]) # sentinel
+      p = p[:-1] + [project(1/2*(p[i] + p[i+1]), newRoot) for i in range(nVerts)]
+      if nVerts == 4:
+        p.append(project(center, newRoot))
+    else:
+      p = p + [project(1/2*(p[i] + newRoot), newRoot) for i in range(nVerts)]      
+    
     # scale down
     scaled = []
-    for vert in projected:
+    for vert in p:
       scaled.append(
-        bm.verts.new(center + 0.02 * (vert - center)))
+        bm.verts.new(newRoot + 0.02 * (vert - newRoot)))
     
-    t = triangle; s = scaled; p = projected
+    t = polygon; s = scaled
     
     bm.faces.new([t[0], s[0], s[2], t[2]])
     bm.faces.new([t[1], s[1], s[0], t[0]])
@@ -50,10 +71,10 @@ def generate_tree_mesh(base_obj, ref_obj, iterations=4):
     bm.faces.new([s[2], s[0], s[5]])
     
     if level > 0:   
-      do_recursive(center, [s[0], s[3], s[5]], level - 1, projected=[p[0], p[3], p[5]])
-      do_recursive(center, [s[1], s[4], s[3]], level - 1, projected=[p[1], p[4], p[3]])
-      do_recursive(center, [s[2], s[5], s[4]], level - 1, projected=[p[2], p[5], p[4]])
-      do_recursive(center, [s[3], s[4], s[5]], level - 1, projected=[p[3], p[4], p[5]])
+      do_recursive(newRoot, [s[0], s[3], s[5]], level - 1, projected=[p[0], p[3], p[5]])
+      do_recursive(newRoot, [s[1], s[4], s[3]], level - 1, projected=[p[1], p[4], p[3]])
+      do_recursive(newRoot, [s[2], s[5], s[4]], level - 1, projected=[p[2], p[5], p[4]])
+      do_recursive(newRoot, [s[3], s[4], s[5]], level - 1, projected=[p[3], p[4], p[5]])
       
     else:  
       bm.faces.new([s[0], s[3], s[5]])
@@ -80,8 +101,8 @@ def generate_tree_mesh(base_obj, ref_obj, iterations=4):
     if len(face.verts) == 3:
       cache.append(list(face.verts))
       
-  for triangle in cache:
-    do_recursive(root, triangle, iterations)
+  for face in cache:
+    do_recursive(root, face, iterations)
   
   tree_mesh = bpy.data.meshes.new('tree_mesh')
   
